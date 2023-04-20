f3b3172a2e8ab1d1068ef0b1a2c02a4dd405c677
krismac12 <91189116+krismac12@users.noreply.github.com>
2023-04-20 16:00:23 +1000

    fetch movie data with search

--- cab_230/src/API-Calls/MovieCalls.js
+++ cab_230/src/API-Calls/MovieCalls.js
@@ -20,22 +20,31 @@ export function fetchPerson(imdbID,token){
     })
 }
 
-export function fetchMovies(pageNumber,movieTitle,releaseYear){
-    const url = "http://sefdb02.qut.edu.au:3000/movies/search"
+export function fetchMovies(pageNumber, movieTitle, releaseYear) {
+    const url = "http://sefdb02.qut.edu.au:3000/movies/search/?";
 
-    return fetch(url + new URLSearchParams({
-        page: pageNumber,
-        title: movieTitle,
-        year: releaseYear
-    }),{
+    const params = {};
+
+    if (pageNumber) {
+        params.page = pageNumber;
+    }
+
+    if (movieTitle) {
+        params.title = movieTitle;
+    }
+
+    if (releaseYear) {
+        params.year = releaseYear;
+    }
+
+    return fetch(url + new URLSearchParams(params), {
         method: 'GET',
         headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json'
         },
-
     })
-    .then(res =>{
-        return res.json()
-    })
+    .then(res => {
+        return res.json();
+    });
 }
--- cab_230/src/Pages/Movies.js
+++ cab_230/src/Pages/Movies.js
@@ -6,9 +6,12 @@ import { Link } from "react-router-dom";
 import { Alert } from "react-bootstrap";
 import Popups from "../Components/Popups";
 import { AgGridReact } from "ag-grid-react";
+import { useState,useEffect } from "react";
+import { fetchMovies } from "../API-Calls/MovieCalls";
+
 import 'ag-grid-community/styles/ag-grid.css';
 import 'ag-grid-community/styles/ag-theme-alpine.css';
-import { useState } from "react";
+
 /*
 Component for rendering the movies search page
 */
@@ -23,14 +26,16 @@ export default function Movies(){
     // Defines wether to display alert and what message to display
     const{display,message,variant} = useSelector(state => state.Alerts)
 
+    const[pageNumber,setPageNumber] = useState(1)
+    const[movieTitle,setMovieTitle] = useState(null)
+    const[movieYear,setMovieYear] = useState(null)
+
     const [rowData,setRowData] = useState([
-        {title: "Title",year: 1982,imdbRating: 7.5,rottenTomatoesRating:98,metacriticRating:99,imdbID: "imasidaasidm"},
-        {title: "Title2",year: 1984,imdbRating: 7.5,rottenTomatoesRating:98,metacriticRating:99},
-        {title: "Title2",year: 1984,imdbRating: 7.5,rottenTomatoesRating:98,metacriticRating:99}
+        {}
     ])
 
     const columnDefs = [
-        {field: 'title',sortable: true, cellRendererFramework: (params)=><div><Link to={`/movies/${params.data.imdbID}`}>{params.value}</Link></div>},
+        {field: 'title',sortable: true, cellRendererFramework: (params)=><div><Link to={`/movies/data/${params.data.imdbID}`}>{params.value}</Link></div>, width: 400 },
         {field: 'year',sortable: true},
         {field: 'imdbRating',sortable: true},
         {field: 'rottenTomatoesRating',sortable: true},
@@ -38,8 +43,22 @@ export default function Movies(){
 
     ]
 
-    const[columns,setColumns] = useState(3)
-    
+
+    useEffect(() => {
+        fetchMovies(1, movieTitle, movieYear)
+            .then(res => {
+                setRowData(res.data);
+                console.log(res)
+            });
+    }, []);
+
+    const handleSearch = (searchValues) => {
+        fetchMovies(1, searchValues.title, searchValues.year)
+          .then((res) => {
+            setRowData(res.data);
+            console.log(res)
+          });
+      }
     return(
         <div id="page">
             <div className="main">
@@ -49,10 +68,19 @@ export default function Movies(){
                     <Link to ="/movie/person/nm0000191" className={disabled ? "disabled-link" : "normal"}>
                         <Button className="text-light" variant="dark" disabled = {disabled}>Person Data</Button>
                     </Link>
-                    <div className="ag-theme-alpine" style={{height: (45*columns + 70), width:'100%'}}>
+                    <div id="Search">
+                        <h4>Title</h4>
+                        <input type= "Text" onChange={(e) => setMovieTitle(e.target.value)}></input>
+                        <h4>Release Year</h4>
+                        <input type="number" min="1900" max="2023" onChange={(e) => setMovieYear(e.target.value)}></input>
+                        <h4>Search</h4>
+                        <Button className="text-light" variant="dark" disabled={disabled} onClick={() => handleSearch({title: movieTitle,year: movieYear})}><h4>Search</h4></Button>
+                    </div>
+                    <div className="ag-theme-alpine" style={{width:'100%'}}>
                         <AgGridReact
                             rowData={rowData}
                             columnDefs={columnDefs}
+                            domLayout="autoHeight"
                         />
                     </div>
                 </div> 

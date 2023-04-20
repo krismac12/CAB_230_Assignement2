85cd0863ad9b5391bc631caacd31c47457c77b29
krismac12 <91189116+krismac12@users.noreply.github.com>
2023-04-20 13:34:49 +1000

    implemented movie data table with no fetch

--- cab_230/src/Pages/Movies.js
+++ cab_230/src/Pages/Movies.js
@@ -5,7 +5,10 @@ import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
 import { Alert } from "react-bootstrap";
 import Popups from "../Components/Popups";
-
+import { AgGridReact } from "ag-grid-react";
+import 'ag-grid-community/styles/ag-grid.css';
+import 'ag-grid-community/styles/ag-theme-alpine.css';
+import { useState } from "react";
 /*
 Component for rendering the movies search page
 */
@@ -20,6 +23,23 @@ export default function Movies(){
     // Defines wether to display alert and what message to display
     const{display,message,variant} = useSelector(state => state.Alerts)
 
+    const [rowData,setRowData] = useState([
+        {title: "Title",year: 1982,imdbRating: 7.5,rottenTomatoesRating:98,metacriticRating:99,imdbID: "imasidaasidm"},
+        {title: "Title2",year: 1984,imdbRating: 7.5,rottenTomatoesRating:98,metacriticRating:99},
+        {title: "Title2",year: 1984,imdbRating: 7.5,rottenTomatoesRating:98,metacriticRating:99}
+    ])
+
+    const columnDefs = [
+        {field: 'title',sortable: true, cellRendererFramework: (params)=><div><Link to={`/movies/${params.data.imdbID}`}>{params.value}</Link></div>},
+        {field: 'year',sortable: true},
+        {field: 'imdbRating',sortable: true},
+        {field: 'rottenTomatoesRating',sortable: true},
+        {field: 'metacriticRating',sortable: true}
+
+    ]
+
+    const[columns,setColumns] = useState(3)
+    
     return(
         <div id="page">
             <div className="main">
@@ -29,6 +49,12 @@ export default function Movies(){
                     <Link to ="/movie/person/nm0000191" className={disabled ? "disabled-link" : "normal"}>
                         <Button className="text-light" variant="dark" disabled = {disabled}>Person Data</Button>
                     </Link>
+                    <div className="ag-theme-alpine" style={{height: (45*columns + 70), width:'100%'}}>
+                        <AgGridReact
+                            rowData={rowData}
+                            columnDefs={columnDefs}
+                        />
+                    </div>
                 </div> 
             </div>
             {/* Renders Alert message */}
